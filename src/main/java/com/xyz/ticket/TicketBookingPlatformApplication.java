ed26d22e81c96b2c98e8214fda34fe98a090b531
Mohit Maan <mohit.maan@gfs.com>
2021-12-09 11:34:41 +0530

    Created SpringBoot App

---
+++ src/main/java/com/xyz/ticket/TicketBookingPlatformApplication.java
@@ -0,0 +1,14 @@
+package com.xyz.ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBookingPlatformApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TicketBookingPlatformApplication.class, args);
+	}
+
+}
